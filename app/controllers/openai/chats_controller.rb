61e7099a4079d1e25dbd1ce8f7e68aa661e9cea4
erijawa <e.akgw0913@gmail.com>
2024-09-03 12:17:46 +0900

    Add:fontawesomeの導入

--- app/controllers/openai/chats_controller.rb
+++ app/controllers/openai/chats_controller.rb
@@ -12,9 +12,10 @@ class Openai::ChatsController < ApplicationController
         parameters: {
           model: "gpt-3.5-turbo",
           messages: [
-            { role: "system", content: "関西弁で返答してください" },
+            { role: "system", content: "あなたは3才の子供です。対話相手の言葉に対し、「〇〇ってなに？」「どうして〇〇なの？」のように質問をしてください。" },
             { role: "user", content: @user_input }
           ],
+          max_tokens: 50,
           temperature: 0.7
         }
       )
